9343d9956d636434d45c84651d808ab12296f102
Taylor Council <councit@gmail.com>
2020-03-27 02:15:54 -0700

    Updates

---
+++ precache-manifest.ebc4487fb3ae48c13a1f529fa5ac980b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bcb02b40c19062fdd920ecaff453b5ca",
+    "url": "/Rolodex/index.html"
+  },
+  {
+    "revision": "61814bf2714e6bbace40",
+    "url": "/Rolodex/static/css/main.9e1af395.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/Rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "61814bf2714e6bbace40",
+    "url": "/Rolodex/static/js/main.b551b17f.chunk.js"
+  },
+  {
+    "revision": "89d65f1b55a4d4bc9b78",
+    "url": "/Rolodex/static/js/runtime-main.599087ae.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/rolodex/precache-manifest.e1cbce6bedb94472b04b3570da529dd2.js"
+  "/Rolodex/precache-manifest.ebc4487fb3ae48c13a1f529fa5ac980b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Rolodex/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
